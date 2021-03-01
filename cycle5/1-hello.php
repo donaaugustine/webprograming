3ef450bb36d6c6a970c59edde3d5c80bc3fbee90
donaaugustine <74652290+donaaugustine@users.noreply.github.com>
2021-03-01 15:58:38 +0530

    Add files via upload

---
+++ cycle5/1-hello.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Hello world</h1>
+<?php
+echo "Hello , ";
+echo "Dona Augustine";
+?>
+
+</body>
+</html>
---
+++ cycle5/4-getdata.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>get data from another page</title>
+</head>
+<body>
+ <?php
+
+echo "Name:" .$_POST["name"]."</br>";
+echo "Roll no.:".$_POST["rollno"]."</br>";
+echo "Address:".$_POST["addr"]."</br>";
+echo "Contact No.:".$_POST["contact"]."</br>";
+echo "Email ID:".$_POST["mail"]."</br>";
+
+ ?>
+</body>
+</html>
